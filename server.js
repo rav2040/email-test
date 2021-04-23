eef9c4b3d3a4bda686ce3bda2e066c95442c7013
Stuart Kennedy <stuart.kennedy@qrious.co.nz>
2021-04-23 13:19:11 +1200

    Add heroku starter

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    console.log(req.headers)
-    res.end("")
-});
-
-server.listen(4000, () => console.log("listening..."))
