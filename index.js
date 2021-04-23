83d30d321e78f8ed3784f9fbe9845899ef99fc4a
Stuart Kennedy <stuart.kennedy@qrious.co.nz>
2021-04-23 13:51:05 +1200

    a

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use((req) => console.log(req.headers))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .use((req) => console.log(req.headers))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
